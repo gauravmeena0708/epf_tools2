bded755c31aac554d28eab6f2cc39061b9d6e509
Gaurav Meena <gauravmeena0708@gmail.com>
2023-11-26 22:40:32 +0530

    Update pdf_generator.py

--- src/epftools/pdf_generator.py
+++ src/epftools/pdf_generator.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from reportlab.lib.pagesizes import letter
 from reportlab.lib import colors
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, PageBreak, Paragraph
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, PageBreak, Paragraph, LineBreak
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter, landscape
 
@@ -62,6 +62,7 @@ class PDFGenerator:
             table = self.convert_df2table(df)
             elements.append(table)
             elements.append(empty_line)
+            elements.append(LineBreak())
             if i < len(dataframes) - 1 and with_break:
                 elements.append(PageBreak())
 
