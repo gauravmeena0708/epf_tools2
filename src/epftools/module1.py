522836de2f3db8c94e2fe0c891e0370df77707de
Gaurav Meena <gauravmeena0708@gmail.com>
2023-11-28 19:45:30 +0530

    Update module1.py
    some example code addition

--- src/epftools/module1.py
+++ src/epftools/module1.py
@@ -20,3 +20,47 @@ class Number(object):
     @classmethod
     def addall(cls, number_obj_iter):
         cls(sum(n.val() for n in number_obj_iter))
+
+
+""" dynamic Label generator
+import pandas as pd
+
+def create_labels(data, bins):
+    labels = []
+
+    for _, row in data.iterrows():
+        for j, bin_range in enumerate(bins):
+            if bin_range[0] <= row['PENDING DAYS'] <= bin_range[1]:
+                labels.append(f'{bin_range[0]}-{bin_range[1]}')
+                break
+
+    return labels
+
+# Create sample data
+data = {'PENDING DAYS': [10, 25, 32, 15, 50,50000,500]}
+df = pd.DataFrame(data)
+
+# Define bins
+bins = [[0, 20], [21, 100], [101, 5000]]
+
+# Create labels using the function
+labels = create_labels(df, bins)
+
+# Print the labels
+print(labels)
+"""
+
+"""
+ClaimProcessor
+PeriodicityProcessor
+EstmstProcessor
+PerformanceProcessor
+PendencyProcessor
+DscProcessor
+EsignProcessor
+TransinProcessor
+OnlineProcessor
+PrimaryProcessor
+OthersProcessor
+pdfkithtmltopdf
+"""
