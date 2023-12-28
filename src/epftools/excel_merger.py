41d39925ae1f178201e4b44cafa36e8ce167d46b
Gaurav Meena <gauravmeena0708@gmail.com>
2023-12-28 22:55:44 +0530

    Update excel_merger.py

--- src/epftools/excel_merger.py
+++ src/epftools/excel_merger.py
@@ -2,15 +2,16 @@ import os
 import pandas as pd
 
 class ExcelMerger:
-    def __init__(self, folder_path):
+    def __init__(self, folder_path, ext=".xlsx", sheetnum=0):
         self.folder_path = folder_path
         self.excel_names = self.get_excel_files()
         self.excels = self.load_excels()
         self.frames = self.extract_frames()
+        self.ext    = ext
+        self.sheet_num = 0
 
     def get_excel_files(self):
-        excel_names = [filename for filename in os.listdir(self.folder_path) if filename.endswith('.xls')]
-        print(excel_names)
+        excel_names = [filename for filename in os.listdir(self.folder_path) if filename.endswith(self.ext)]
         return excel_names
 
     def load_excels(self):
@@ -23,8 +24,7 @@ class ExcelMerger:
         return excels
 
     def extract_frames(self):
-        frames = [x.parse(x.sheet_names[0], header=None, index_col=None) for x in self.excels]
-
+        frames = [x.parse(x.sheet_names[self.sheet_num], header=None, index_col=None) for x in self.excels]
         # Delete the first row for all frames except the first
         # i.e. remove the header row -- assumes it's the first
         frames[1:] = [df[1:] for df in frames[1:]]
@@ -34,7 +34,6 @@ class ExcelMerger:
     def merge_and_save(self, output_filename="merged.xlsx"):
         # Concatenate the frames
         combined = pd.concat(self.frames)
-
         # Write it out to Excel
         combined.to_excel(output_filename, header=False, index=False)
         print(f"Merged Excel files saved to {output_filename}")
