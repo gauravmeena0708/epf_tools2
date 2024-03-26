b5f3cf363c14600593f21acff17ff110dd860713
Gaurav Meena <gauravmeena0708@gmail.com>
2024-03-26 17:44:56 +0530

    combining with the periodicity processor

--- tests/todo/ml_categorization.py
+++ tests/todo/ml_categorization.py
@@ -1,44 +1,49 @@
-
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+from sklearn.impute import SimpleImputer
+from epftools import PeriodicityProcessor as pp
+import pandas as pd
 
-# Assuming df is your training dataframe with 'text_column' and 'reason_category'
-# columns, and df_all2 is your testing dataframe with 'text_column' and 'reason_category' columns
 
-# Read your CSV file for training
-df = pd.read_csv("reason_category.csv")
-df['reason'] = df['reason'].fillna('')
+pd.options.mode.copy_on_write = True
+path = 'data/2024.csv'
+dall = pp.read_periodicity(path, '2023-24')
+temp_df = pd.DataFrame(columns=['text_column', 'blank', 'reason1', 'reason2', 'reason_category'])
+temp_df[['blank', 'reason1', 'reason2']] = dall['REJECT_REASON'].str.split(r'\d\)', n=2, expand=True)
 
-# Vectorize the text_column for training
+dall['reason1'] = temp_df['reason1'].str.strip()
+dall['reason2'] = temp_df['reason2'].str.strip()
+dall['days_taken'] = dall['DAYS_TAKEN_FOR_REJECTION'].combine_first(dall['DAYS_TAKEN_FOR_SETTLEMENT'])
+dall = dall.drop(['DAYS_TAKEN_FOR_REJECTION', 'DAYS_TAKEN_FOR_SETTLEMENT'], axis=1)
+dall['days_category'] = pd.cut(dall['days_taken'],
+                             bins=[0, 5, 10, 15, 20, float('inf')],
+                             labels=['0-5', '6-10', '11-15', '16-20', '>20'],
+                             right=False)
+dall2 = dall[dall['outcome'] == 'rejected']
+dall2.head()
+
+df = pd.read_csv("F:\\git\\epftools\\tests\\todo\\reason_category.csv")
+df['reason'] = df['reason'].fillna(' ')
+df['category'] = df['category'].fillna(' ')
 vectorizer = TfidfVectorizer()
 X_train = vectorizer.fit_transform(df['reason'])
 y_train = df['category']
 
-# Model selection and training
+
 model = LogisticRegression()
 model.fit(X_train, y_train)
 
-
-
-# Handle missing values in the 'reason' column for testing
+"""
+For ML based Rejection categorization
+"""
 dall2['reason1'] = dall2['reason1'].fillna('').copy()
-
-# Vectorize the text_column for testing
 X_all2 = vectorizer.transform(dall2['reason1'])
-
-# Prediction on the entire dataset df_all2
 all2_predictions = model.predict(X_all2)
-
-# Probability estimates for each class on the entire dataset df_all2
 all2_probabilities = model.predict_proba(X_all2)
-
-# Get the predicted category and confidence based on the maximum probability on df_all2
 dall2['predicted_category'] = all2_predictions.copy()
 dall2['confidence'] = all2_probabilities.max(axis=1).copy()
-
-# Display the dataframe df_all2 with predicted categories and confidence
 display(dall2.head())
 cols = ["GROUP_ID","TASK_ID","EST","CLAIM_ID","PARA_DETAILS","MEMBER_ID","reason1","predicted_category","confidence"]
-dall2[(dall2['STATUS']=='Rejected') & (dall2['FORM_NAME']=='31')][cols].to_excel("december1.xlsx",index=False)
+dall2[(dall2['STATUS']=='Rejected')][cols].to_excel("rejection_cat.xlsx",index=False)
